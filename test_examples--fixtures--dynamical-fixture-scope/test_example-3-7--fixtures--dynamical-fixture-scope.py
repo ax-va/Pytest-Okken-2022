3bf8f232c94859dc68a5d297192f95e0aacc7b18
ax-va <vasiliev.informatik@t-online.de>
2024-03-09 16:51:02 +0100

    update

---
+++ test_examples--fixtures--dynamical-fixture-scope/test_example-3-7--fixtures--dynamical-fixture-scope.py
@@ -0,0 +1,45 @@
+import cards
+
+
+def test_empty(cards_db):
+    assert cards_db.count() == 0
+
+
+def test_two(cards_db):
+    cards_db.add_card(cards.Card("first"))
+    cards_db.add_card(cards.Card("second"))
+    assert cards_db.count() == 2
+
+
+# The fixture scope of "cards_db" is "session"
+"""
+$ pytest --setup-show -k test_example-3-7
+===================================================================================================== test session starts ======================================================================================================
+...                                                                                                                                                                               
+
+test_examples--fixtures--dynamical-fixture-scope/test_example-3-7--fixtures--dynamical-fixture-scope.py 
+SETUP    S cards_db
+        test_examples--fixtures--dynamical-fixture-scope/test_example-3-7--fixtures--dynamical-fixture-scope.py::test_empty (fixtures used: cards_db).
+        test_examples--fixtures--dynamical-fixture-scope/test_example-3-7--fixtures--dynamical-fixture-scope.py::test_two (fixtures used: cards_db).
+TEARDOWN S cards_db
+
+=============================================================================================== 2 passed, 32 deselected in 0.04s ===============================================================================================
+"""
+
+# The fixture scope of "cards_db" is "function".
+# "--func-db" is specified in conftest.py.
+"""
+$ pytest --func-db --setup-show -k test_example-3-7
+===================================================================================================== test session starts ======================================================================================================
+...                                                                                                                                                                              
+
+test_examples--fixtures--dynamical-fixture-scope/test_example-3-7--fixtures--dynamical-fixture-scope.py 
+        SETUP    F cards_db
+        test_examples--fixtures--dynamical-fixture-scope/test_example-3-7--fixtures--dynamical-fixture-scope.py::test_empty (fixtures used: cards_db).
+        TEARDOWN F cards_db
+        SETUP    F cards_db
+        test_examples--fixtures--dynamical-fixture-scope/test_example-3-7--fixtures--dynamical-fixture-scope.py::test_two (fixtures used: cards_db).
+        TEARDOWN F cards_db
+
+=============================================================================================== 2 passed, 32 deselected in 0.04s ===============================================================================================
+"""
