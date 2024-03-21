e0a8dcdcf3c5c1e3c880d8e11c4bc414bf2b4a8e
ax-va <vasiliev.informatik@t-online.de>
2024-03-21 19:13:58 +0100

    update

--- test_examples-04--buildin-fixtures/test_example-04-2--buildin-fixtures--capsys.py
+++ test_examples-04--buildin-fixtures/test_example-04-2--buildin-fixtures--capsys.py
@@ -1,11 +1,10 @@
 """
-How to test outputs to stdout, stderr, and so on.
 The capsys fixture enables the capturing of writes to stdout and stderr.
-Also, it can disable normal output capture from pytest by using the -s or --capture=no flags.
+Also, "with capsys.disabled():" disables normal output capturing without using the -s or --capture=no flags.
 """
 import cards
 
-# Motivation: test the command-line output:
+# Motivation 1: test the command-line output
 # $ cards version
 # 1.0.0
 import subprocess
@@ -23,11 +22,36 @@ def test_version_v1():
 
 def test_version_v2(capsys):
     cards.cli.version()
-    # The capsys.readouterr() method returns a namedtuple that has out and err
-    output = capsys.readouterr().out.rstrip()
+    # capsys.readouterr() returns a namedtuple that has out and err
+    output = capsys.readouterr().out.rstrip()  # rstrip() to remove the newline
     assert output == cards.__version__
 
 
+# Motivation 2:
+# The output is not displayed, because pytest captures all the outputs.
+# To show print(), use the -s or --capture=no flags in the command line.
 def test_normal():
-    # We don't see the output, because pytest captures all the outputs
     print("\nnormal print")
+
+
+# However, print() is displayed in a failing test.
+def test_fail():
+    print("\nprint in failing test")
+    assert False
+
+
+# Alternatively, disable capturing to display print() without using additional flags.
+def test_disabled(capsys):
+    with capsys.disabled():
+        print("\ncapsys disabled print")
+
+
+# See also:
+# - capfd
+# is like capsys, but captures file descriptors 1 and 2, which usually is the same as stdout and stderr.
+# - capsysbinary
+# capsys captures text, capsysbinary captures bytes.
+# - capfdbinary
+# captures bytes on file descriptors 1 and 2.
+# - caplog
+# captures output written with the logging package
