b6de1d913db77fe367ff5ea9f4aadd75cb67d5e9
ax-va <vasiliev.informatik@t-online.de>
2024-09-04 12:28:15 +0200

    update

--- 10--mocking/conftest.py
+++ 10--mocking/conftest.py
@@ -17,20 +17,3 @@ def mock_cards_db():
         db = CardsDB.return_value
         db.path.return_value = "/foo/"
         yield db
-
-
-# Create fixture with temporary database
-@pytest.fixture(scope="session")
-def cards_db():
-    """ CardsDB object connected to a temporary database """
-    with TemporaryDirectory() as db_dir:
-        print("db_dir:", db_dir)
-        print("Here")
-        # SETUP
-        db_path = Path(db_dir)
-        db = cards.CardsDB(db_path)
-        # Pass to tests
-        yield db
-        # TEARDOWN
-        db.close()
-    # Clean up the temporary directory
